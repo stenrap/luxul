949610cd87ed450760986c180cbbc0b3bbdbf0df
Rob Johansen <robert_johans22@hotmail.com>
2016-12-03 22:02:03 -0700

    Implement feature request #4 (sytling).

--- src/containers/Wireless/ProfileTable.js
+++ src/containers/Wireless/ProfileTable.js
@@ -2,49 +2,52 @@ import React from 'react';
 import { Table, TableBody, TableHeader, TableRow, TableHeaderColumn, TableRowColumn} from 'material-ui/Table';
 import IconButton from 'material-ui/IconButton';
 import DeleteForever from 'material-ui/svg-icons/action/delete-forever';
+import Paper from 'material-ui/Paper';
 
 export default class ProfileTable extends React.Component {
     render() {
         const { profiles, handleDelete } = this.props;
 
         return (
-            <Table className="profileTable" selectable={false}>
-                <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
-                    <TableRow>
-                        <TableHeaderColumn>Band</TableHeaderColumn>
-                        <TableHeaderColumn>SSID</TableHeaderColumn>
-                        <TableHeaderColumn>SSID Broadcast</TableHeaderColumn>
-                        <TableHeaderColumn>Client Isolation</TableHeaderColumn>
-                        <TableHeaderColumn>Security Mode</TableHeaderColumn>
-                        <TableHeaderColumn>Actions</TableHeaderColumn>
-                    </TableRow>
-                </TableHeader>
-                <TableBody displayRowCheckbox={false}>
-                    {profiles.map((profile, i) => {
-                        return (
-                            <TableRow key={i}>
-                                <TableRowColumn>{profile.radio}</TableRowColumn>
-                                <TableRowColumn>{profile.ssid}</TableRowColumn>
-                                <TableRowColumn>
-                                    {profile.ssidBroadcast ? "Enabled" : "Disabled"}
-                                </TableRowColumn>
-                                <TableRowColumn>
-                                    {profile.clientIsolation ? "Enabled" : "Disabled"}
-                                </TableRowColumn>
-                                <TableRowColumn>{profile.security}</TableRowColumn>
-                                <TableRowColumn>
-                                    <IconButton
-                                        tooltip="Delete"
-                                        onTouchTap={() => handleDelete(i)}
-                                    >
-                                        <DeleteForever />
-                                    </IconButton>
-                                </TableRowColumn>
-                            </TableRow>
-                        );
-                    })}
-                </TableBody>
-            </Table>
+            <Paper className="profileTable" zDepth={4}>
+                <Table selectable={false}>
+                    <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+                        <TableRow>
+                            <TableHeaderColumn>Band</TableHeaderColumn>
+                            <TableHeaderColumn>SSID</TableHeaderColumn>
+                            <TableHeaderColumn>SSID Broadcast</TableHeaderColumn>
+                            <TableHeaderColumn>Client Isolation</TableHeaderColumn>
+                            <TableHeaderColumn>Security Mode</TableHeaderColumn>
+                            <TableHeaderColumn>Actions</TableHeaderColumn>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody displayRowCheckbox={false}>
+                        {profiles.map((profile, i) => {
+                            return (
+                                <TableRow key={i}>
+                                    <TableRowColumn>{profile.radio}</TableRowColumn>
+                                    <TableRowColumn>{profile.ssid}</TableRowColumn>
+                                    <TableRowColumn>
+                                        {profile.ssidBroadcast ? "Enabled" : "Disabled"}
+                                    </TableRowColumn>
+                                    <TableRowColumn>
+                                        {profile.clientIsolation ? "Enabled" : "Disabled"}
+                                    </TableRowColumn>
+                                    <TableRowColumn>{profile.security}</TableRowColumn>
+                                    <TableRowColumn>
+                                        <IconButton
+                                            tooltip="Delete"
+                                            onTouchTap={() => handleDelete(i)}
+                                        >
+                                            <DeleteForever />
+                                        </IconButton>
+                                    </TableRowColumn>
+                                </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                </Table>
+            </Paper>
         )
     }
 }
--- src/containers/Wireless/RadioConfiguration.js
+++ src/containers/Wireless/RadioConfiguration.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Toggle from 'material-ui/Toggle';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
+import Paper from 'material-ui/Paper';
 
 export default class RadioConfiguration extends React.Component {
     render() {
@@ -31,69 +32,71 @@ export default class RadioConfiguration extends React.Component {
         } = this.props;
         
         return (
-            <div className="radioConfiguration">
-                <h3>Radio Configuration</h3>
-                <Toggle
-                    label="2.4 GHz Enabled"
-                    toggled={radio24Enabled}
-                    onToggle={ (event, value) => handleRadioToggle("2.4GHz", value) }
-                />
-                <SelectField
-                    disabled={!radio24Enabled}
-                    floatingLabelText="2.4 GHz Channel Width"
-                    value={radio24Width}
-                    onChange={ (event, key, value) => handleRadioChannelWidth("2.4GHz", value) }
-                    fullWidth
-                >
-                    <MenuItem value="20 MHz" primaryText="20 MHz" />
-                    <MenuItem value="20/40 MHz Auto" primaryText="20/40 MHz Auto" />
-                </SelectField>
-                <SelectField
-                    disabled={!radio24Enabled}
-                    floatingLabelText={primaryLabel}
-                    value={radio24Channel}
-                    onChange={ (event, key, value) => handleRadioChannel("2.4GHz", value) }
-                    fullWidth
-                >
-                    {[...Array(11)].map((x, i) => <MenuItem key={i+1} value={i+1} primaryText={i+1} /> )}
-                </SelectField>
-                <SelectField
-                    disabled={!radio24Enabled}
-                    className={hideSecondary ? 'hidden' : ''}
-                    floatingLabelText="Secondary 2.4 GHz Channel"
-                    value={secondaryChannel}
-                    onChange={ (event, key, value) => handleSecondaryRadioChannel("2.4GHz", value) }
-                    fullWidth
-                >
-                    <MenuItem value="Above" primaryText="Above" disabled={!aboveEnabled} />
-                    <MenuItem value="Below" primaryText="Below" disabled={!belowEnabled} />
-                </SelectField>
-                <Toggle
-                    label="5 GHz Enabled"
-                    toggled={radio5Enabled}
-                    onToggle={ (event, value) => handleRadioToggle("5GHz", value) }
-                />
-                <SelectField
-                    disabled={!radio5Enabled}
-                    floatingLabelText="5 GHz Channel Width"
-                    value={radio5Width}
-                    onChange={ (event, key, value) => handleRadioChannelWidth("5GHz", value) }
-                    fullWidth
-                >
-                    <MenuItem value="20 MHz" primaryText="20 MHz" />
-                    <MenuItem value="20/40 MHz Auto" primaryText="20/40 MHz Auto" />
-                    <MenuItem value="20/40/80 MHz Auto" primaryText="20/40/80 MHz Auto" />
-                </SelectField>
-                <SelectField
-                    disabled={!radio5Enabled}
-                    floatingLabelText="5 GHz Channel"
-                    value={radio5Channel}
-                    onChange={ (event, key, value) => handleRadioChannel("5GHz", value) }
-                    fullWidth
-                >
-                    {validChannels.map((x, i) => <MenuItem key={x} value={x} primaryText={x} />)}
-                </SelectField>
-            </div>
+            <Paper className="radioConfiguration" zDepth={4}>
+                <div>
+                    <h3>Radio Configuration</h3>
+                    <Toggle
+                        label="2.4 GHz Enabled"
+                        toggled={radio24Enabled}
+                        onToggle={ (event, value) => handleRadioToggle("2.4GHz", value) }
+                    />
+                    <SelectField
+                        disabled={!radio24Enabled}
+                        floatingLabelText="2.4 GHz Channel Width"
+                        value={radio24Width}
+                        onChange={ (event, key, value) => handleRadioChannelWidth("2.4GHz", value) }
+                        fullWidth
+                    >
+                        <MenuItem value="20 MHz" primaryText="20 MHz" />
+                        <MenuItem value="20/40 MHz Auto" primaryText="20/40 MHz Auto" />
+                    </SelectField>
+                    <SelectField
+                        disabled={!radio24Enabled}
+                        floatingLabelText={primaryLabel}
+                        value={radio24Channel}
+                        onChange={ (event, key, value) => handleRadioChannel("2.4GHz", value) }
+                        fullWidth
+                    >
+                        {[...Array(11)].map((x, i) => <MenuItem key={i+1} value={i+1} primaryText={i+1} /> )}
+                    </SelectField>
+                    <SelectField
+                        disabled={!radio24Enabled}
+                        className={hideSecondary ? 'hidden' : ''}
+                        floatingLabelText="Secondary 2.4 GHz Channel"
+                        value={secondaryChannel}
+                        onChange={ (event, key, value) => handleSecondaryRadioChannel("2.4GHz", value) }
+                        fullWidth
+                    >
+                        <MenuItem value="Above" primaryText="Above" disabled={!aboveEnabled} />
+                        <MenuItem value="Below" primaryText="Below" disabled={!belowEnabled} />
+                    </SelectField>
+                    <Toggle
+                        label="5 GHz Enabled"
+                        toggled={radio5Enabled}
+                        onToggle={ (event, value) => handleRadioToggle("5GHz", value) }
+                    />
+                    <SelectField
+                        disabled={!radio5Enabled}
+                        floatingLabelText="5 GHz Channel Width"
+                        value={radio5Width}
+                        onChange={ (event, key, value) => handleRadioChannelWidth("5GHz", value) }
+                        fullWidth
+                    >
+                        <MenuItem value="20 MHz" primaryText="20 MHz" />
+                        <MenuItem value="20/40 MHz Auto" primaryText="20/40 MHz Auto" />
+                        <MenuItem value="20/40/80 MHz Auto" primaryText="20/40/80 MHz Auto" />
+                    </SelectField>
+                    <SelectField
+                        disabled={!radio5Enabled}
+                        floatingLabelText="5 GHz Channel"
+                        value={radio5Channel}
+                        onChange={ (event, key, value) => handleRadioChannel("5GHz", value) }
+                        fullWidth
+                    >
+                        {validChannels.map((x, i) => <MenuItem key={x} value={x} primaryText={x} />)}
+                    </SelectField>
+                </div>
+            </Paper>
         )
     }
 }
