708e376e913e158421692acaee4de88608ff40a9
Rob Johansen <robert_johans22@hotmail.com>
2016-12-03 20:59:53 -0700

    Implement the delete confirmation dialog (feature request #3).

--- src/containers/Wireless/Wireless.js
+++ src/containers/Wireless/Wireless.js
@@ -5,11 +5,53 @@ import RadioConfiguration from './RadioConfiguration';
 import ProfileForm from './ProfileForm';
 import ProfileTable from './ProfileTable';
 import { setRadioEnabled, setRadioChannelWidth, setRadioChannel, setSecondaryRadioChannel, addProfile, deleteProfile } from '../../redux/wireless';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 
 class Wireless extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            confirmDelete: false,
+            profileToDelete: -1
+        };
+    }
+    
+    handleOpenDialog(profile) {
+        this.setState({
+            confirmDelete: true,
+            profileToDelete: profile
+        });
+    }
+    
+    handleCloseDialog() {
+        this.setState({
+            confirmDelete: false,
+            profileToDelete: -1
+        });
+    }
+    
+    handleDelete() {
+        this.props.dispatch(deleteProfile(this.state.profileToDelete));
+        this.handleCloseDialog();
+    }
+    
     render() {
         const { profiles, radios, dispatch } = this.props;
-
+        const actions = [
+            <FlatButton
+                label="Delete"
+                keyboardFocused={true}
+                primary={true}
+                onTouchTap={() => this.handleDelete()}
+            />,
+            <FlatButton
+                label="Cancel"
+                primary={true}
+                onTouchTap={() => this.handleCloseDialog()}
+            />
+        ];
         return (
             <div className="wirelessPage">
                 <RadioConfiguration
@@ -24,8 +66,16 @@ class Wireless extends React.Component {
                 />
                 <ProfileTable
                     profiles={profiles}
-                    handleDelete={ (i) => dispatch(deleteProfile(i)) }
+                    handleDelete={ (i) => this.handleOpenDialog(i) }
                 />
+                <Dialog
+                    title="Please Confirm"
+                    actions={actions}
+                    open={this.state.confirmDelete}
+                    onRequestClose={() => this.handleCloseDialog()}
+                >
+                    Are you sure you want to delete this profile?
+                </Dialog>
             </div>
         )
     }
